a6ec5ff876afcd8174b076976836e966e09a1b66
aakash kishan <aakash_kishan@outlook.com>
2017-07-20 19:57:13 +0530

    Mobile Friendly

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $('#video').videoBG({
     mp4:'assets/fly.mp4',
     //ogv:'assets/bg.ogv',
     webm:'assets/fly.webm',
-    // poster:'assets/banner5.jpg',
+    poster:'assets/poster1.jpg',
     scale:true,
     zIndex:0,
     height: vidHeight
